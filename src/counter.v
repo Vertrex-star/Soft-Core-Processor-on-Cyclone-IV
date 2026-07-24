64cba0f4acff6c189dc31ae8391119958ed6b9bb
Paul Vincent Bezzina <109743463+Vertrex-star@users.noreply.github.com>
2026-07-24 19:16:03 +0300

    Update counter.v

--- src/counter.v
+++ src/counter.v
@@ -1,9 +1,18 @@
-module count #(parameter int inc)(input[7:0] count, output[7:0] countNext);
+module count #(parameter inc = 1)
+	  (
+	  input rst,
+	  input[7:0] count, 
+	  output[7:0] countNext  
+	  );
 
-reg[7:0] r_countrNex;
+reg[7:0] r_countNext;
 
 always @(count) begin
-	r_countNext = count + inc; 
+	if(rst == 1) begin 
+		r_countNext <= 0;
+	end else begin 
+	  r_countNext = count + inc;
+	end
 end
 
 assign countNext = r_countNext;
