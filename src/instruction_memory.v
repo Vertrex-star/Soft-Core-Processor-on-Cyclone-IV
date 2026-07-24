a368cb5bc1bc7856ea5a66830b4d579d225e136c
Paul Vincent Bezzina <109743463+Vertrex-star@users.noreply.github.com>
2026-07-24 19:17:16 +0300

    Update instruction_memory.v

--- src/instruction_memory.v
+++ src/instruction_memory.v
@@ -1,30 +1,27 @@
-module instruction_memory 
+module instruction_memory
 (
-	input wr,
-	input rd,
-	input[23:0] i_instructions,
-	input[7:0] PC, 
-	input clk,
-	output[23:0] o_instructions
+    input        wr,
+    input        rd,
+    input [23:0] i_instructions,
+    input [7:0]  PC,
+    input        clk,
+    output [23:0] o_instructions
 );
 
-reg [23:0] mem [7:0];
-reg [23:0] out;
+    reg [23:0] mem [0:7];
+    reg [23:0] out;
+    integer N = 0;
 
-integer N = 0;
+    always @(posedge clk) begin
+        if (wr == 1'b1 && ~rd && (i_instructions != 24'd0) && (N < 8)) begin
+            mem[N] <= i_instructions;
+            N <= N + 1;
+        end
+        else if (rd == 1'b1 && ~wr) begin
+            out <= mem[PC-1];         
+        end
+    end
 
-always @(posedge clk) begin 
-	if (wr == 1'b1 && ~rd && ~(i_instructions == 0) && N < 8) begin
-		mem[N] <= i_instructions;
-		N <= N + 1;
-		end
-	end 
-	else if (rd == 1'b1 && ~wr) begin 
-		out <= mem[PC-1];
-		mem[PC - 1] = 0;
-	end
-end
+    assign o_instructions = out;
 
-assign o_instructions = out;
-									
 endmodule
